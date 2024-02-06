1a027d797fceaf9e56f01f0a8f28f9dd2d86ae99
孙率众 <334837455@qq.com>
2024-02-06 09:13:49 +0800

    1.重复代码优化 2.采集器优化

--- src/main/java/com/wanda/epc/device/ClientHandler.java
+++ src/main/java/com/wanda/epc/device/ClientHandler.java
@@ -1,26 +1,22 @@
 package com.wanda.epc.device;
 
-import com.wanda.epc.common.SpringUtil;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@ChannelHandler.Sharable
+@Slf4j
 @Component
 public class ClientHandler extends ChannelInboundHandlerAdapter {
-
-    private final static Logger logger = LoggerFactory.getLogger(ClientHandler.class);
 
     //数据类别 1：轮询命令（ENQ ） 数值：0xE0
     //数据类别 2：回应数据（DAT ） 数值：0xA0
     private static byte ctrl_Data = (byte) 0xa0;
 
-    private static Device slDevice;
-
+    @Autowired
+    Device device;
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
@@ -30,29 +26,28 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
             // 复制内容到字节数组bytes
             buf.readBytes(buff);
             // 将接收到的数据转为字符串，此字符串就是客户端发送的字符串
-            logger.info("接收到的数据:" + ByteUtil.hex2String(buff));
+            log.info("接收到的数据:" + ByteUtil.hex2String(buff));
             int dataSize = ByteUtil.bytesToShort(buff, 0);
             //数据长度2个byte 控制码1个byte 校验码一个byte 电梯字节12byte
             if (buff.length % 12 != 4 || dataSize != buff.length) {
-                logger.info("长度不对");
+                log.info("长度不对");
                 return;
             } else if (buff[2] != ctrl_Data) {
-                logger.info("收到的控制码不正确，获得数据失败！");
+                log.info("收到的控制码不正确，获得数据失败！");
                 return;
-            } else if (cRC(buff)) {
+            } else if (CRC(buff)) {
                 //格式验证完成，开始解析
                 byte[] temp = new byte[12];
                 for (int i = 3; i < buff.length - 12; i = i + 12) {
                     System.arraycopy(buff, i, temp, 0, 12);
-                    slDevice = SpringUtil.getBean(Device.class);
-                    slDevice.oneInfo(temp);
+                    device.oneInfo(temp);
                 }
             } else {
-                logger.info("校验失败！");
+                log.info("校验失败！");
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("数据处理异常", e);
         }
 
     }
@@ -64,7 +59,7 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
      * @param b
      * @return 是否校验成功
      */
-    private boolean cRC(byte[] b) {
+    private boolean CRC(byte[] b) {
 
         byte crcByte = 0;
         for (int i = 0; i < b.length - 1; i++) {
@@ -75,7 +70,7 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
         if ((b[b.length - 1] & 0xff) == (crcByte & 0xff)) {
             return true;
         }
-        logger.info("CRC" + (crcByte & 0xff) + ",得到的数据为：" + (b[b.length - 1] & 0xff));
+        log.info("CRC" + (crcByte & 0xff) + ",得到的数据为：" + (b[b.length - 1] & 0xff));
         return false;
     }
 
---
+++ src/main/java/com/wanda/epc/device/NettyClient.java
@@ -0,0 +1,63 @@
+package com.wanda.epc.device;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+@Slf4j
+@Component
+public class NettyClient {
+
+    @Resource
+    ClientHandler clientHandler;
+    @Value("${tcp.serverIP}")
+    private String serverIP;
+    @Value("${tcp.port}")
+    private Integer port;
+
+    private static byte[] base = {(byte) 0x00, (byte) 0x04, (byte) 0xe0, (byte) 0x1b};
+
+    @PostConstruct
+    private void init() {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            /**客户端相关配置信息*/
+            Bootstrap bootstrap = new Bootstrap();
+            //绑定线程组
+            bootstrap.group(workerGroup);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) {
+                    ch.pipeline().addLast(clientHandler);
+                }
+            });
+            ChannelFuture future = bootstrap.connect(serverIP, port).sync();
+            //请求报文
+            ByteBuf byteBufMsg = Unpooled.buffer();
+            byteBufMsg.writeBytes(base);
+            future.channel().writeAndFlush(byteBufMsg);
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("初始化异常：", e);
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+}
